fefe61e3a5a483d76647203cf16108bafb8a3185
unknown <timmortal44@yahoo.com>
2023-12-03 22:39:57 +0000

    get out some comments

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,10 @@
 import bcrypt from "bcrypt"; // allows for password encryption
 import jwt from "jsonwebtoken"; // sends a user a web token they will use for authorization
-import User from "../models/User.js"; // Model -> how the data will be structured in MongoDB
+import User from "../models/User.js";
 
 /* REGISTER USER */
 export const register = async (req, res) => { // making a call to the database -> so async
-  // req -> is coming from the frontend
-  // res -> is what the server sends back
+
   try {
     const {
       firstName,
