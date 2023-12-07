9e6dc67c6075cf96aa875fca7df3db47f1eefaa4
unknown <timmortal44@yahoo.com>
2023-12-07 22:58:07 +0000

    auth cotrollers all cleaned up

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,7 +2,7 @@ import bcrypt from "bcrypt";``
 import jwt from "jsonwebtoken";
 import User from "../models/User.js";
 
-/* REGISTER USER */
+
 export const register = async (req, res) => { 
 
   try {
@@ -40,11 +40,9 @@ export const register = async (req, res) => {
   }
 };
 
-/* LOGGING IN */
 export const login = async (req, res) => {
   try {
     const { email, password } = req.body;
-    //  use mongoose to find that specified email
     const user = await User.findOne({ email: email });
     if (!user) return res.status(400).json({ msg: "User does not exist. " });
 
