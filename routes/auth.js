d74313ddf9b54021543d290e20c98e0cb7ccf8a2
unknown <timmortal44@yahoo.com>
2023-12-20 21:47:29 +0000

    knify the spaces now

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,5 @@
 import express from "express";
 import { login } from "../controllers/auth.js";
 const router = express.Router();
-
-// he didn't specify auth here because app.use (is already applying the auth in index js)
 router.post("/login", login);
-
-
 export default router;
