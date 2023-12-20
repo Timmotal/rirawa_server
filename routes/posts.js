68247ecaa36ca27e1bd6c9315a009e784607af3d
unknown <timmortal44@yahoo.com>
2023-12-20 21:47:58 +0000

    do it for the posts route

--- routes/posts.js
+++ routes/posts.js
@@ -3,12 +3,6 @@ import { getFeedPosts, getUserPosts, likePost } from "../controllers/posts.js";
 import { verifyToken } from "../middleware/auth.js";
 
 const router = express.Router();
-
-/* READ */
- // grabs user feed when on their homepage
-//  the homepage takes every single post in the database and serves it
-// IF YOU WANT THIS TO BE CURATED -> YOU WOULD NEED A VERY SOPHISTICATED ALGORITHM
-// NOW THEY USE AI & ML TO MAKE SURE THOSE FEEDS ARE RELEVANT AND PROVIDING VALUE TO THE ADVERTISERS TOO
 router.get("/", verifyToken, getFeedPosts);
 // grab only posts a particular user posted 
 router.get("/:userId/posts", verifyToken, getUserPosts); // says this is redundant
