ffac3484179dfdf2c839772775ce2d98280ffdf5
unknown <timmortal44@yahoo.com>
2023-12-20 21:47:07 +0000

    renove oute the auth explainers

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,5 @@
 import express from "express";
 import { login } from "../controllers/auth.js";
-
-// allows Express to recognize that these routes will be configured
-// allows us to have it in separate files -> keeping us organized 
 const router = express.Router();
 
 // he didn't specify auth here because app.use (is already applying the auth in index js)
