94345135e672ab9f37f1327c0b72dfcdfab11608
unknown <timmortal44@yahoo.com>
2023-12-20 21:48:15 +0000

    clean up real well

--- routes/posts.js
+++ routes/posts.js
@@ -5,9 +5,7 @@ import { verifyToken } from "../middleware/auth.js";
 const router = express.Router();
 router.get("/", verifyToken, getFeedPosts);
 // grab only posts a particular user posted 
-router.get("/:userId/posts", verifyToken, getUserPosts); // says this is redundant
-// what he said was redundant was actually perfect, but I think I am the one who maybe wrong here tho
-// router.get("/:userId", verifyToken, getUserPosts);
+router.get("/:userId/posts", verifyToken, getUserPosts);
 
 /* UPDATE */
 router.patch("/:id/like", verifyToken, likePost); // like and unlike a post
