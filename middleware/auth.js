98be0559cacab6e66e7b127cb633e9a28920f58e
unknown <timmortal44@yahoo.com>
2023-12-23 22:38:39 +0000

    some more sxtras for the auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,6 @@
 import jwt from "jsonwebtoken";
 
-export const verifyToken = async (req, res, next) => { // next allows us to have the function continued
+export const verifyToken = async (req, res, next) => {
   try {
     let token = req.header("Authorization");
 
@@ -8,12 +8,9 @@ export const verifyToken = async (req, res, next) => { // next allows us to have
       return res.status(403).send("Access Denied");
     }
 
-    if (token.startsWith("Bearer ")) { // (00:56:40)
-      // take everything from the right side of Bearer, thats how we grab the actual token
-      token = token.slice(7, token.length).trimLeft(); // se trim the space also because we added spaces
+    if (token.startsWith("Bearer ")) {
+      token = token.slice(7, token.length).trimLeft(); 
     }
-
-    // this is where we use that secret string you do not want anybody to know
     const verified = jwt.verify(token, process.env.JWT_SECRET);
     req.user = verified;
     next(); // run the next function
